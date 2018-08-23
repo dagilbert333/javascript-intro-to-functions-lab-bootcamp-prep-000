bcb1edd8a77589e8a76aef0456319c12e1747cb6
David Gilbert <dagilbert333@gmail.com>
2018-08-23 20:15:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string){
+  return string.toUpperCase();
+}
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function logShout(string){
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string){
+  var uppercase = string.toUpperCase()
+  var loveCheck = "I love you, Grandma."
+  
+  if (string !== uppercase) {
+  return "I can't hear you!"
+ } else {
+  return "YES INDEED!"
+}
+}
+
+ function loveCheck(){
+   if (sayHiToGrandma() === "I love you, Grandma."){
+   return "I love you, too."
+ }
+}
